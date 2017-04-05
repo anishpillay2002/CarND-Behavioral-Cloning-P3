de22fd218090f9a56af504e16c6073fcd6fcf886
anishpillay2002 <anishpillay2002@gmail.com>
2017-04-04 21:54:48 -0500

    Adding additional training data

--- drive.py
+++ drive.py
@@ -3,7 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
-
+import cv2
 import numpy as np
 import socketio
 import eventlet
@@ -60,7 +60,11 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
+        image_array=image.crop((0,65,320,150))
+        image_array=image_array.resize((200,66))
+        image_array = np.asarray(image_array)
+        #image_array=image_array[65:150,0:320]
+        #image_array=np.resize(image_array,(200,66))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
